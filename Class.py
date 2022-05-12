48f311ef51a2b8d195741fd8aeccdb4ee388275a
Florian Delrieu <florian.delrieu@free.fr>
2022-05-12 21:54:47 +0200

    [v0.8] - new functions
    - add set_velocity
    - change CI
    - change plot

--- Class.py
+++ Class.py
@@ -1,4 +1,5 @@
-import numpy as np
+import numpy
+from numpy import sqrt,arctan,cos,sin
 
 g = 10
 Rho = 1.225 # air density at sea level (bar)
@@ -9,20 +10,23 @@ class Ballistic_Object:
         self.mass = m
         self.surface = surface
         self.drag_coefficient = drag
+
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
         self.ax = 0
         self.ay = 0
+#        self.angle = arctan(self.vx/self.vy)
 
-        self.V = np.sqrt(self.vx**2 + self.vy**2)
-        self.A = np.sqrt(self.ax**2 + self.ay**2)
+        self.V = sqrt(self.vx**2 + self.vy**2)
+        self.A = sqrt(self.ax**2 + self.ay**2)
 
         self.color = "r"
         self.Name = ""
 
         self.list_t = []
+        self.list_angle = []
         self.list_x = []
         self.list_y = []
         self.list_vx = []
@@ -30,8 +34,14 @@ class Ballistic_Object:
         self.list_ax = []
         self.list_ay = []
 
+    def set_velocity(self,V,alpha):
+        alpha = numpy.deg2rad(alpha)
+        self.vx = V*cos(alpha)
+        self.vy = V*sin(alpha)
+
     def save_kinetics(self,t):
         self.list_t.append(t)
+#        self.angle = arctan(self.vx/self.vy)
         self.list_x.append(self.x)
         self.list_y.append(self.y)
         self.list_vx.append(self.vx)
