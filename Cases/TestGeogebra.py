e8c1fe70b061c7b7fa891ed89d927a6e7c277c5a
Florian Delrieu <florian.delrieu@free.fr>
2022-05-17 22:52:20 +0200

    [v0.8A.3] - testing
    - bug report

--- Cases/TestGeogebra.py
+++ Cases/TestGeogebra.py
@@ -7,17 +7,26 @@ Y0 = 0
 V0 = 10
 alpha0 = 45
 
+tf = 10
+dt = 0.01
+
 V0x = V0*cos(alpha0)
 V0y = V0*sin(alpha0)
 
-x = arange(0,10,0.1)
-y = lambda x: -1/2*g * x**2/V0x**2 + V0y/V0x*x + Y0
+#x1 = arange(0,10,0.1)
+#y1 = lambda x: -1/2*g * x**2/V0x**2 + V0y/V0x*x + Y0
 
 A = Ballistic_Object(1,0,Y0,V0x,V0y,drag=0)
-Solve(A,0.01,10)
+Solve(A,dt,tf)
+
+t = arange(0,tf,dt)
+x = lambda t: V0x*t
+y = lambda t: -1/2*g*t**2 + V0y*t + Y0
+
 
 plt.figure("XY plot")
-plt.plot(x,y(x),"k--",label="Analytique")
+plt.plot(x(t),y(t),"k--",label="Analytique")
+#plt.plot(x1,y1(x1),"g--",label="y(x)")
 plt.plot(A.list_x,A.list_y,"b-",label="Simulation")
 
 plt.legend(loc="best")
